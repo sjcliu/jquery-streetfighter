3f3d2789be7ed05ba73482c87dc70e8b325afc61
sjcliu <sjcliu@gmail.com>
2015-01-04 00:28:22 +1100

    sound

--- js/app.js
+++ js/app.js
@@ -11,14 +11,27 @@ $(document).ready(function() {
 	})
 
 	.mousedown(function() {
-		//play houdouken sound
+		playHadouken();
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
 		$('.hadouken').show();
-		// show hadouken and animate it to the right of the screen
+		$('.hadouken').finish().show()
+		.animate(
+			{'left': '300px'},
+			500,
+			function() {
+				$(this).hide();
+				$(this).css('left', '-127px')
+			});
 	})
 	.mouseup(function() {
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
 	});
 });
+
+function playHadouken() {
+	$('#hadouken-sound')[0].volume = 0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}
